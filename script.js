fac5ffe2fa6cb5a3d974525ed5cb8e92da9eeb1e
jacint697 <155924683+jacint697@users.noreply.github.com>
2026-09-24 09:28:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ function calcular() {
     nota1trim = Number(prompt ("digite a nota do primeiro trimestre"));
      nota2trim = Number(prompt ("digite a nota do segundo trimestre"));
 
-     resultado = 180 - nota1trim + nota2trim;
+     resultado = 180 - (nota1trim + nota2trim);
 
-     
-}
+    if(resultado <= 0){
+        alert("parabens!, Voce está aprovado, mas eu ainda quero voltar pra ela");
+    } else {
+        alert("você ainda precisa tirar" + resultado + "no terceiro trimestere para se aprovado, davi gay");
+    }
+
+    }
